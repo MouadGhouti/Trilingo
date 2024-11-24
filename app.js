3eaf6628eb29d74896d7e9bd937c098a67d77d15
MouadGhouti <137650892+MouadGhouti@users.noreply.github.com>
2024-11-24 18:23:58 +0300

    Converted course-list to ejs and created half the back end for it
    Still need to add back end for the forum we have

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+//Database models
+const  Courses = require('./assests/js/course.js'); // Sequelize model
+console.log('Courses from app.js', Courses);
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -10,15 +13,26 @@ app.set('views', 'views');
 app.use('/assets', express.static(path.join(__dirname, 'assests')));
 app.use('/componentes', express.static(path.join(__dirname, 'componentes')));
 
-app.get('/course-list', () => { 
-//load data from db)
-    Course.findAll()
-// render template with data
-    .then((data) => {
-        response.render('template', { courses: data})
-        
-    }) 
+
+
+app.get('/course-list', async (req, res) => {
+    // try {
+    //     const courses = await Course.findAll(); 
+    //     res.render('course-list', { courses });
+    // } catch (error) {
+    //     res.status(500).send('Internal Server Error');
+    // }
+    res.render('course-list', { Courses });
 });
+// app.get('/course-list', () => { 
+// //load data from db)
+//     Course.findAll()
+// // render template with data
+//     .then((data) => {
+//         response.render('template', { courses: data})
+        
+//     }) 
+// });
 
 
 app.get('/', (req, res) => {
